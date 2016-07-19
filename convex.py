02fee580b43ace38db083aa8131979f09e45adc6
GamrCorps <GamrCorps@gmail.com>
2016-07-18 20:13:09 -0700

    Fixed list literals

--- convex.py
+++ convex.py
@@ -90,30 +90,30 @@ def split_code(code):
         line_stack = []
         block = ''
         index = 0
-        open = []
+        open_literals = []
         while index < len(line):
             if line[index] == '{':
-                open.append('}')
+                open_literals.append('}')
                 block += '{'
                 index += 1
             elif line[index] == '[':
-                open.append(']')
+                open_literals.append(']')
                 block += '['
                 index += 1
-            elif open:
-                if line[index] == '\\' and open[len(open)-1] == '"' and line[index + 1] == '"':
+            elif open_literals:
+                if line[index] == '\\' and open_literals[len(open_literals)-1] == '"' and line[index + 1] == '"':
                     block += '\\"'
                     index += 2
                     continue
                 block += line[index]
-                if line[index] == open[len(open)-1]:
-                    open.pop()
-                    if len(open) == 0:
+                if line[index] == open_literals[len(open_literals)-1]:
+                    open_literals.pop()
+                    if len(open_literals) == 0:
                         line_stack.append(block)
                         block = ''
                 index += 1
             elif line[index] == '"':
-                open.append('"')
+                open_literals.append('"')
                 block += '"'
                 index += 1
             elif line[index] == '\'':
@@ -123,6 +123,7 @@ def split_code(code):
                 line_stack.append(line[index])
                 index += 1
         result.append(line_stack)
+        # print(result)
     return result
 
 
@@ -143,8 +144,51 @@ def run_op(name):
 
 
 def run_temp_stack(code):
-    # run by pulling from temp stack first, then external stack
-    return
+    temp_stack = []
+    code_stack = split_code(code)[0]
+
+    def push_temp_stack(x):
+        if x is not None:
+            temp_stack.append(x)
+
+    def pop_temp_stack():
+        if len(temp_stack) > 0:
+            return temp_stack.pop()
+        else:
+            return stack.pop()
+
+    def run_op_temp_stack(name):
+        arity = operators[name]['arity']
+        if arity == 1:
+            a = pop_temp_stack()
+            return operators[name]['call'](a)
+        elif arity == 2:
+            a = pop_temp_stack()
+            b = pop_temp_stack()
+            return operators[name]['call'](b, a)
+        elif arity == 3:
+            a = pop_temp_stack()
+            b = pop_temp_stack()
+            c = pop_temp_stack()
+            return operators[name]['call'](c, b, a)
+    index = 0
+    while index < len(code_stack):
+        if len(code_stack[index]) == 1:
+            if re.match("[A-Z¢è]", code_stack[index]):
+                push_temp_stack(variables[code_stack[index]])
+                index += 1
+            else:
+                push_temp_stack(run_op_temp_stack(code_stack[index]))
+                index += 1
+        else:
+            if code_stack[index][0] == '"':
+                push_temp_stack(exec(code_stack[index]))
+            elif code_stack[index][0] == '\'':
+                push_temp_stack(Char(code_stack[index]))
+            elif code_stack[index][0] == '[':
+                push_temp_stack(run_temp_stack(code_stack[index][1:len(code_stack[index]) - 1]))
+            index += 1
+    return temp_stack
 
 
 def run(code):
@@ -164,8 +208,8 @@ def run(code):
                 push(exec(code_stack[current_line][index]))
             elif code_stack[current_line][index][0] == '\'':
                 push(Char(code_stack[current_line][index]))
-            else:
-                push(run_temp_stack(code_stack[current_line][index]))
+            elif code_stack[current_line][index][0] == '[':
+                push(run_temp_stack(code_stack[current_line][index][1:len(code_stack[current_line][index])-1]))
             index += 1
 
 
@@ -176,9 +220,9 @@ Operator Functions
 """
 
 
-def base_convert(list, base):
-    if is_number(list) and is_list(base):
-        return base_convert(base, list)
+def base_convert(list_a, base):
+    if is_number(list_a) and is_list(base):
+        return base_convert(base, list_a)
 
 
 variables = {
@@ -213,6 +257,10 @@ variables = {
 }
 
 operators = {
+    ' ': attrdict(
+        arity=0,
+        call=None
+    ),
     'a': attrdict(
         arity=1,
         call=lambda x: [x]
@@ -224,5 +272,5 @@ operators = {
 }
 
 
-run('[][1 2 3]')
+run('[][A B[C]]')
 print(stack)
